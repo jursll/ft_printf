4a0b0cb6cce5c46d4b9bc3c3c1531a1e4918aef5
Julia Russell <juliarussell@MBP-de-Julia.sunrise.box>
2024-10-19 15:22:25 +0200

    19.10 15:22 H

--- Makefile
+++ Makefile
@@ -6,7 +6,32 @@
 #    By: julrusse <marvin@42lausanne.ch>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/19 12:32:42 by juliarussel       #+#    #+#              #
-#    Updated: 2024/10/19 12:36:37 by julrusse         ###   ########.fr        #
+#    Updated: 2024/10/19 15:10:33 by julrusse         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME	= libftprintf.a
+CFLAGS	= -Wall -Wextra -Werror
+CC		= gcc
+
+FILES	= ft_printf.c \
+
+OBJ		= $(FILES:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	ar rcs $(NAME) $(OBJ)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean:	clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all, clean, fclean, re
---
+++ libftprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julrusse <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 15:12:39 by julrusse          #+#    #+#             */
+/*   Updated: 2024/10/19 15:15:55 by julrusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include <stdarg.h>
+
+int	ft_printf(const char *format, ...)
+
+#endif
