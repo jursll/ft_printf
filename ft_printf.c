a8842b7cc65babed3fdd88bd9b398e29d932bbd5
Julia Russell <juliarussell@MBP-de-Julia.sunrise.box>
2024-10-21 15:34:01 +0200

    21.10 15:33 H

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,30 @@
 /*   By: julrusse <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 14:57:30 by julrusse          #+#    #+#             */
-/*   Updated: 2024/10/20 22:02:23 by julrusse         ###   ########.fr       */
+/*   Updated: 2024/10/21 15:32:54 by julrusse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+static int	ft_select_arg(char c, va_list *args)
+{
+	int	count;
+
+	count = 0;
+	if (c == 'c')
+		count = ft_putchar_count((char)va_arg(*args, int));
+	else if (c == 's')
+		count = ft_putstr_count(va_arg(*args, char *));
+	return (count);
+}
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int		i;
 	int		count;
+	int		result;
 
 	va_start(args, format);
 	i = 0;
@@ -26,19 +39,12 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-				ft_c((char)va_arg(args, int));
-				count++;
-			else if (format[i] == 's')
-				ft_s((char *)va_arg(args, char *));
-				count += ft_strlen((char *)va_arg(args, char *));
+			result = ft_select_arg(format[i], &args);
+			count += result;
 		}
 		else
-		{
-			ft_putstr_fd(format[i], 1);
-			i++;
-			count++;
-		}
+			count += ft_putstr_count(format, &count);
+		i++;
 	}
 	va_end(args);
 	return (count);
--- ft_words_count.c
+++ ft_words_count.c
@@ -1,25 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   csp_functions.c                                    :+:      :+:    :+:   */
+/*   ft_words_count.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: julrusse <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 20:07:48 by julrusse          #+#    #+#             */
-/*   Updated: 2024/10/20 20:11:24 by julrusse         ###   ########.fr       */
+/*   Updated: 2024/10/21 15:29:01 by julrusse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_c(char c)
+int	ft_putchar_count(char c)
 {
 	ft_putchar_fd(c, 1);
 	return (1);
 }
 
-int	ft_s(char *s)
+int	ft_putstr_count(char *s)
 {
-	ft_putstr_fd(s, 1);
-	return (1);
+	size_t	count;
+	if (!s)
+	{
+		write(1, "(null)", 6);
+	}
+	count = 0;
+	while (s[count])
+	{
+		ft_putchar_fd(s, 1);
+		count++;
+	}
+
+	return (count);
 }
